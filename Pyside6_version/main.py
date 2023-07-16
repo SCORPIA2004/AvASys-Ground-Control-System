a829b75eeed23ebc4e365cc9a3757c4471d1d507
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-07-16 14:39:27 +0300

    Added pages

--- Pyside6_version/main.py
+++ Pyside6_version/main.py
@@ -1,11 +1,12 @@
-import sys, io, folium
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
-from PySide6.QtQuickWidgets import QQuickWidget
-from PySide6.QtWebEngineWidgets import QWebEngineView
+import folium
+import io
+import sys
+import csv
+
 from PySide6.QtWidgets import *
+
 from ui_main import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,61 +16,46 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.username = ""
         self.password = ""
-        # self.ui.stackedWidget.setCurrentIndex(0)
-        self.ui.stackedWidgetMain.setCurrentIndex(1)
 
+        # Start window at Login page
+        self.ui.stackedWidgetMain.setCurrentIndex(0)
 
-        # Start assigning functions to widgets here
+        # Start assigning functions to login page widgets here
         self.ui.pushButtonLogin.clicked.connect(self.logUserIn)
         self.ui.lineEditUsername.textChanged.connect(self.setUsername)
         self.ui.lineEditPassword.textChanged.connect(self.setPassword)
 
-        mapView = QQuickWidget()
-        mapView.setSource(QUrl.fromLocalFile("test_qml_box.qml"))
-        self.ui.stackedWidgetMain.addWidget(mapView)
+
+        # start assigning functions to menu page widgets here
+        self.showMap()
+        self.ui.pushButtonFlightData.clicked.connect(self.gotoFlightDataPage)
+        self.ui.pushButtonSetup.clicked.connect(self.gotoSetupPage)
+        self.ui.pushButtonConfig.clicked.connect(self.gotoConfigPage)
+        self.ui.pushButtonHelp.clicked.connect(self.gotoHelpPage)
 
 
-        coordinate = (39.865506432608136, 32.74609830979015)
 
-        m = folium.Map(
-            title="Bilka hill",
-            zoom_start=13,
-            location=coordinate
-        )
-
-        # save map data
-
-        # folium.CircleMarker(
-        #     location=coordinate,
-        #     radius=5,
-        #     popup="Bilkent Hill",
-        #     color="red",
-        #     fill=True,
-        #     fill_color="red"
-        # ).add_to(m)
-        icon_path = "plane.png"
-        icon_size = (50, 50)
-        # custom_icon_html = f'<img src="{icon_path}" style="width:{icon_size[0]}px;height:{icon_size[1]}px;">'
-        # folium.Marker(coordinate, icon=folium.DivIcon(html=custom_icon_html)).add_to(m)
-
-        folium.Marker(
-            location=coordinate,
-            popup="Bilkent Hill",
-            icon=folium.Icon(color="red", icon="info-sign")
-        ).add_to(m)
-
-        data = io.BytesIO()
-        m.save(data, close_file=False)
-        html_content = data.getvalue().decode()
-
-        # mapWebView = QWebEngineView()
-        self.ui.webEngineViewMap.setHtml(html_content)
-
-
+        # mapView = QQuickWidget()
+        # self.ui.webEngineViewMap.setSource(QUrl.fromLocalFile("test_qml_box.qml"))
+        # self.ui.stackedWidgetMenu.addWidget(self.ui.webEngineViewMap)
 
     def logUserIn(self):
-        if self.username == "admin" and self.password == "admin":
-            self.ui.stackedWidget.setCurrentIndex(1)
+        # open csv file and check if username and password are correct
+        with open('credentials.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                # print(f"\trow[0] BUT {self.username}")
+                if self.username == row[0] and self.password == row[1]:
+                    self.ui.stackedWidgetMain.setCurrentIndex(1)
+                line_count += 1
+
+        # if self.username == "admin" and self.password == "admin":
+        #     self.ui.stackedWidgetMain.setCurrentIndex(1)
+        print("No such user")
+
+
 
     def setUsername(self, s):
         self.username = s
@@ -77,11 +63,50 @@ class MainWindow(QMainWindow):
     def setPassword(self, s):
         self.password = s
 
+    def showMap(self):
+        coordinate = (39.865506432608136, 32.74609830979015)
+        m = folium.Map(
+            title="Bilka hill",
+            zoom_start=13,
+            location=coordinate
+        )
+        # TODO: change the icon of marker to a plane
+        icon_path = "plane.png"
+        icon_size = (50, 50)
+        # custom_icon_html = f'<img src="{icon_path}" style="width:{icon_size[0]}px;height:{icon_size[1]}px;">'
+        # folium.Marker(coordinate, icon=folium.DivIcon(html=custom_icon_html)).add_to(m)
+        # End of TODO1
+        folium.Marker(
+            location=coordinate,
+            popup="Bilkent Hill",
+            icon=folium.Icon(color="red", icon="info-sign")
+        ).add_to(m)
 
 
-if __name__ == '__main__':
+        data = io.BytesIO()
+        m.save(data, close_file=False)
+        html_content = data.getvalue().decode()
+
+        self.ui.webEngineViewMap.setHtml(html_content)
+
+    def gotoFlightDataPage(self):
+        self.ui.stackedWidgetMenu.setCurrentIndex(1)
+
+    def gotoSetupPage(self):
+        self.ui.stackedWidgetMenu.setCurrentIndex(2)
+
+    def gotoConfigPage(self):
+        self.ui.stackedWidgetMenu.setCurrentIndex(3)
+
+    def gotoHelpPage(self):
+        self.ui.stackedWidgetMenu.setCurrentIndex(4)
+
+
+
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGL)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
--- Pyside6_version/ui_main.py
+++ Pyside6_version/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainsYypZc.ui'
+## Form generated from reading UI file 'mainxVilpA.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.2
 ##
@@ -16,9 +16,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,
-    QPushButton, QSizePolicy, QSpacerItem, QStackedWidget,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QSpacerItem, QStackedWidget, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -80,9 +80,19 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.lineEditUsername, 3, 0, 1, 1)
 
+        self.lineEditPassword = QLineEdit(self.pageLogin)
+        self.lineEditPassword.setObjectName(u"lineEditPassword")
+        self.lineEditPassword.setMaximumSize(QSize(200, 35))
+
+        self.gridLayout.addWidget(self.lineEditPassword, 4, 0, 1, 1)
+
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.gridLayout.addItem(self.verticalSpacer, 1, 0, 1, 1)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer_2, 7, 0, 1, 1)
 
         self.pushButtonLogin = QPushButton(self.pageLogin)
         self.pushButtonLogin.setObjectName(u"pushButtonLogin")
@@ -90,21 +100,27 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.pushButtonLogin, 5, 0, 1, 1, Qt.AlignHCenter)
 
-        self.lineEditPassword = QLineEdit(self.pageLogin)
-        self.lineEditPassword.setObjectName(u"lineEditPassword")
-        self.lineEditPassword.setMaximumSize(QSize(200, 35))
+        self.pushButtonSignup = QPushButton(self.pageLogin)
+        self.pushButtonSignup.setObjectName(u"pushButtonSignup")
+        self.pushButtonSignup.setMaximumSize(QSize(70, 35))
 
-        self.gridLayout.addWidget(self.lineEditPassword, 2, 0, 1, 1)
-
-        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_2, 6, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButtonSignup, 6, 0, 1, 1, Qt.AlignHCenter)
 
         self.stackedWidgetMain.addWidget(self.pageLogin)
         self.page_2Menu = QWidget()
         self.page_2Menu.setObjectName(u"page_2Menu")
         self.gridLayout_2 = QGridLayout(self.page_2Menu)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.label = QLabel(self.page_2Menu)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 0, 4, 1, 1)
+
+        self.pushButtonFlightData = QPushButton(self.page_2Menu)
+        self.pushButtonFlightData.setObjectName(u"pushButtonFlightData")
+
+        self.gridLayout_2.addWidget(self.pushButtonFlightData, 0, 0, 2, 1)
+
         self.stackedWidgetMenu = QStackedWidget(self.page_2Menu)
         self.stackedWidgetMenu.setObjectName(u"stackedWidgetMenu")
         self.page_3 = QWidget()
@@ -113,36 +129,126 @@ class Ui_MainWindow(object):
         self.webEngineViewMap.setObjectName(u"webEngineViewMap")
         self.webEngineViewMap.setGeometry(QRect(0, 0, 1000, 530))
         self.webEngineViewMap.setUrl(QUrl(u"about:blank"))
+        self.pushButtonMapZoomIn = QPushButton(self.page_3)
+        self.pushButtonMapZoomIn.setObjectName(u"pushButtonMapZoomIn")
+        self.pushButtonMapZoomIn.setGeometry(QRect(10, 20, 50, 50))
+        self.pushButtonMapZoomOut = QPushButton(self.page_3)
+        self.pushButtonMapZoomOut.setObjectName(u"pushButtonMapZoomOut")
+        self.pushButtonMapZoomOut.setGeometry(QRect(10, 80, 50, 50))
+        self.pushButtonCurrentLocation = QPushButton(self.page_3)
+        self.pushButtonCurrentLocation.setObjectName(u"pushButtonCurrentLocation")
+        self.pushButtonCurrentLocation.setGeometry(QRect(10, 140, 50, 50))
         self.stackedWidgetMenu.addWidget(self.page_3)
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.label_3 = QLabel(self.page_1)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(410, 200, 121, 21))
+        self.stackedWidgetMenu.addWidget(self.page_1)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.label_4 = QLabel(self.page_2)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(410, 200, 121, 21))
+        self.stackedWidgetMenu.addWidget(self.page_2)
+        self.page_4 = QWidget()
+        self.page_4.setObjectName(u"page_4")
+        self.label_5 = QLabel(self.page_4)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(410, 200, 121, 21))
+        self.stackedWidgetMenu.addWidget(self.page_4)
+        self.page_5 = QWidget()
+        self.page_5.setObjectName(u"page_5")
+        self.label_6 = QLabel(self.page_5)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(410, 200, 121, 21))
+        self.stackedWidgetMenu.addWidget(self.page_5)
 
-        self.gridLayout_2.addWidget(self.stackedWidgetMenu, 2, 0, 1, 4)
+        self.gridLayout_2.addWidget(self.stackedWidgetMenu, 2, 0, 1, 6)
 
-        self.pushButton_3 = QPushButton(self.page_2Menu)
-        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButtonSetup = QPushButton(self.page_2Menu)
+        self.pushButtonSetup.setObjectName(u"pushButtonSetup")
 
-        self.gridLayout_2.addWidget(self.pushButton_3, 0, 2, 1, 1)
+        self.gridLayout_2.addWidget(self.pushButtonSetup, 0, 1, 2, 1)
 
-        self.pushButton_2 = QPushButton(self.page_2Menu)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.comboBoxSerial = QComboBox(self.page_2Menu)
+        self.comboBoxSerial.setObjectName(u"comboBoxSerial")
 
-        self.gridLayout_2.addWidget(self.pushButton_2, 0, 3, 1, 1)
+        self.gridLayout_2.addWidget(self.comboBoxSerial, 1, 5, 1, 1)
 
-        self.pushButton = QPushButton(self.page_2Menu)
-        self.pushButton.setObjectName(u"pushButton")
+        self.label_2 = QLabel(self.page_2Menu)
+        self.label_2.setObjectName(u"label_2")
 
-        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label_2, 1, 4, 1, 1)
 
-        self.pushButton_4 = QPushButton(self.page_2Menu)
-        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButtonConfig = QPushButton(self.page_2Menu)
+        self.pushButtonConfig.setObjectName(u"pushButtonConfig")
 
-        self.gridLayout_2.addWidget(self.pushButton_4, 0, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.pushButtonConfig, 0, 2, 2, 1)
+
+        self.comboBoxComm = QComboBox(self.page_2Menu)
+        self.comboBoxComm.setObjectName(u"comboBoxComm")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.comboBoxComm.sizePolicy().hasHeightForWidth())
+        self.comboBoxComm.setSizePolicy(sizePolicy1)
+
+        self.gridLayout_2.addWidget(self.comboBoxComm, 0, 5, 1, 1)
+
+        self.pushButtonHelp = QPushButton(self.page_2Menu)
+        self.pushButtonHelp.setObjectName(u"pushButtonHelp")
+
+        self.gridLayout_2.addWidget(self.pushButtonHelp, 0, 3, 2, 1)
 
         self.stackedWidgetMain.addWidget(self.page_2Menu)
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.gridLayout_3 = QGridLayout(self.page)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.verticalSpacer_3 = QSpacerItem(20, 224, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer_3, 0, 1, 1, 1)
+
+        self.lineEditUsernameNew = QLineEdit(self.page)
+        self.lineEditUsernameNew.setObjectName(u"lineEditUsernameNew")
+        self.lineEditUsernameNew.setMaximumSize(QSize(200, 35))
+
+        self.gridLayout_3.addWidget(self.lineEditUsernameNew, 1, 1, 1, 1)
+
+        self.lineEditPasswordNew = QLineEdit(self.page)
+        self.lineEditPasswordNew.setObjectName(u"lineEditPasswordNew")
+        self.lineEditPasswordNew.setMaximumSize(QSize(200, 35))
+
+        self.gridLayout_3.addWidget(self.lineEditPasswordNew, 2, 1, 1, 1)
+
+        self.pushButtonSignupNew = QPushButton(self.page)
+        self.pushButtonSignupNew.setObjectName(u"pushButtonSignupNew")
+        self.pushButtonSignupNew.setMaximumSize(QSize(70, 35))
+
+        self.gridLayout_3.addWidget(self.pushButtonSignupNew, 4, 1, 1, 1, Qt.AlignHCenter)
+
+        self.verticalSpacer_4 = QSpacerItem(20, 60, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer_4, 5, 1, 1, 1)
+
+        self.stackedWidgetMain.addWidget(self.page)
         MainWindow.setCentralWidget(self.centralwidget)
+        QWidget.setTabOrder(self.lineEditUsername, self.lineEditPassword)
+        QWidget.setTabOrder(self.lineEditPassword, self.pushButtonLogin)
+        QWidget.setTabOrder(self.pushButtonLogin, self.pushButtonFlightData)
+        QWidget.setTabOrder(self.pushButtonFlightData, self.pushButtonSetup)
+        QWidget.setTabOrder(self.pushButtonSetup, self.pushButtonConfig)
+        QWidget.setTabOrder(self.pushButtonConfig, self.pushButtonHelp)
+        QWidget.setTabOrder(self.pushButtonHelp, self.pushButtonMapZoomIn)
+        QWidget.setTabOrder(self.pushButtonMapZoomIn, self.comboBoxComm)
+        QWidget.setTabOrder(self.comboBoxComm, self.pushButtonMapZoomOut)
+        QWidget.setTabOrder(self.pushButtonMapZoomOut, self.pushButtonCurrentLocation)
+        QWidget.setTabOrder(self.pushButtonCurrentLocation, self.comboBoxSerial)
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidgetMain.setCurrentIndex(1)
+        self.stackedWidgetMain.setCurrentIndex(0)
         self.stackedWidgetMenu.setCurrentIndex(0)
 
 
@@ -151,10 +257,25 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"AvASys v1.2", None))
+        self.lineEditUsername.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Username", None))
+        self.lineEditPassword.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
         self.pushButtonLogin.setText(QCoreApplication.translate("MainWindow", u"Login", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButtonSignup.setText(QCoreApplication.translate("MainWindow", u"Sign up", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Comm", None))
+        self.pushButtonFlightData.setText(QCoreApplication.translate("MainWindow", u"Flight Data", None))
+        self.pushButtonMapZoomIn.setText(QCoreApplication.translate("MainWindow", u"+", None))
+        self.pushButtonMapZoomOut.setText(QCoreApplication.translate("MainWindow", u"-", None))
+        self.pushButtonCurrentLocation.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Flight data page", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Setup page", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Config page", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Help page", None))
+        self.pushButtonSetup.setText(QCoreApplication.translate("MainWindow", u"Setup", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Serial", None))
+        self.pushButtonConfig.setText(QCoreApplication.translate("MainWindow", u"Config", None))
+        self.pushButtonHelp.setText(QCoreApplication.translate("MainWindow", u"Help", None))
+        self.lineEditUsernameNew.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Username", None))
+        self.lineEditPasswordNew.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
+        self.pushButtonSignupNew.setText(QCoreApplication.translate("MainWindow", u"Sign up", None))
     # retranslateUi
 
