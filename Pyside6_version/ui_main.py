82ef52065faba5a2cf4f4210343717c0c41158be
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-07-19 18:16:57 +0300

    Modified plane stats

--- Pyside6_version/ui_main.py
+++ Pyside6_version/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainEuCruO.ui'
+## Form generated from reading UI file 'mainfmPjXW.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.2
 ##
